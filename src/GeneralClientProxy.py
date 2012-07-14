a0571a3a08720f44612736bc83ea92d9dc386c47
edemirci <emindemirci@gmail.com>
2012-07-14 14:57:34 +0300

    re

--- src/GeneralClientProxy.py
+++ src/GeneralClientProxy.py
@@ -1,4 +1,23 @@
 from ProxyHelper import ProxyHelper
 class GeneralClientProxy:
+	FLAG = 0
 	def __init__(self,connection):
 		self.__proxyHelper = ProxyHelper(connection)
+	def destroy(self,type,name):
+		return self.__proxyHelper.doOp("DESTROY", GeneralClientProxy.FLAG, 0, None, type, name)
+	def txnBegin(self):
+		return self.__proxyHelper.doOp("TRXBEGIN", GeneralClientProxy.FLAG, 0, None)
+	def txnCommit(self):
+		return self.__proxyHelper.doOp("TRXCOMMIT", GeneralClientProxy.FLAG, 0, None)
+	def txnRollback(self):
+		return self.__proxyHelper.doOp("TRXROLLBACK", GeneralClientProxy.FLAG, 0, None)
+	def instances(self):
+		raise NotImplementedError
+	def members(self):
+		raise NotImplementedError
+	def clusterTime(self):
+		raise NotImplementedError
+	def ping(self):
+		raise NotImplementedError
+	def partitions(self):
+		raise NotImplementedError
--- src/GeneralCommandsProxy.py
+++
@@ -1,5 +0,0 @@
-from ProxyHelper import ProxyHelper
-class GeneralCommandsProxy:
-	def __init__(self,connection):
-		self.__proxyHelper = ProxyHelper(connection)
-	
