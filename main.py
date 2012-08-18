b9be557044537e783577f569dddc42b8f67b47fe
Emin Demirci <emindemirci@gmail.com>
2012-08-18 02:45:36 +0200

    main.py deleted

--- main.py
+++
@@ -1,9 +0,0 @@
-import cPickle as pickle    
-
-def serializeObject(pythonObj):
-    return pickle.dumps(pythonObj, pickle.HIGHEST_PROTOCOL)
-
-def deSerializeObject(pickledObj):
-    return pickle.loads(pickledObj)
-
-print type(deSerializeObject(list(serializeObject("test"))))
